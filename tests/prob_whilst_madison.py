13bb09f07b7266dc8657f8e666d7db49082695d1
cs187staff <dengyuntian@g.harvard.edu>
2020-09-16 23:14:12 +0000

    automatic update

--- tests/prob_whilst_madison.py
+++ tests/prob_whilst_madison.py
@@ -1,1 +1,3 @@
-test = {'name': 'prob_whilst_madison', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'prob_whilst_madison',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> 0 <= prob_whilst_madison <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
